a56e98da65394cc3eec38aa485cf00a27be1b211
code.developer13 <dev13@codecraft.com>
2021-08-02 16:04:15 -0400

    Connection to postgresql database

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:123@localhost/height_collector'
+
+db = SQLAlchemy(app)
+
+
+class Data(db.Model):
+    __tablename__ = 'data'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+    height = db.Column(db.Integer)
+
+    def __init__(self, email, height):
+        self.email = email
+        self.height = height
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/success', methods=['POST'])
+def success():
+    if request.method != 'POST':
+        return
+    email = request.form["email_input"]
+    if db.session.query(Data).filter(Data.email == email).count() != 0:
+        return render_template('index.html',
+                               text="Seems like we've got something from that email address already")
+    height = request.form["height_input"]
+    data = Data(email, height)
+    db.session.add(data)
+    db.session.commit()
+    return render_template('success.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
